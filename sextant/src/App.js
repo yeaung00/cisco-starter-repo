43fc4a59f62039947ffcc7ce8bbf908cc688217f
yeaung00 <yep.aung00@gmail.com>
2022-06-30 17:07:17 -0700

    Added title and container

---
+++ sextant/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h1>
+          Sextant
+        </h1>
+      </header>
+      <div className = "container">
+        
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
